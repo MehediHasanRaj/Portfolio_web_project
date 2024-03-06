2779c5a8583ccfe718610bc87ad99254f032b853
MEHEDI HASAN RAJ <mehedihasanraj007@gmail.com>
2024-03-06 20:30:25 +0000

    Update settings.py

--- Portfolio_web_project/settings.py
+++ Portfolio_web_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-km!wi9f8gjq4^7_6$d!!dzy_07@ywo=4_n0@9d^s(yy1m18r1i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
